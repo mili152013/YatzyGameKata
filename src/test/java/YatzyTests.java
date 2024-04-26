25b0a34db8d5ad59aff874619c3c6ac4249687d5
UF624MMI <mohamed.mili@avisto.com>
2024-04-26 11:46:10 +0200

    Add Exceptions TU

--- src/test/java/YatzyTests.java
+++ src/test/java/YatzyTests.java
@@ -1,15 +1,30 @@
-import org.junit.jupiter.api.Test;
 import kata.Yatzy.Yatzy;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
- class YatzyTests {
-    @Test
-     void allDiceSame() {
-        assertEquals(50, Yatzy.scoreYatzy(4, 4, 4, 4, 4));
-    }
+class YatzyTests {
+  @Test
+  void allDiceSame() {
+    assertEquals(50, Yatzy.scoreYatzy(4, 4, 4, 4, 4));
+  }
 
-    @Test
-     void notAllDiceSame() {
-        assertEquals(0, Yatzy.scoreYatzy(4, 4, 4, 4, 3));
-    }
+  @Test
+  void notAllDiceSame() {
+    assertEquals(0, Yatzy.scoreYatzy(4, 4, 4, 4, 3));
+  }
+
+  @Test
+  void throwsExceptionForInvalidDiceValues() {
+    assertThrows(IllegalArgumentException.class, () -> {
+      Yatzy.scorePair(0, 7, 8, -1, 10);
+    });
+  }
+
+  @Test
+  void scoreZeroWhenNoApplicableDice() {
+    assertEquals(0, Yatzy.scoreTwos(1, 2, 3, 4, 5));
+  }
+
 }
